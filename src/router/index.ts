dff938cf08119350435f46d3c5efb6e4f9ecfef6
AbdelRhman <68133113+AbdelRhman125@users.noreply.github.com>
2022-05-02 07:54:29 +0200

    Part 3

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,19 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
+import WelcomePage from '../views/WelcomePage.vue'
 import HomePage from '../views/HomePage.vue'
+import AboutPage from '../views/AboutPage.vue'
 
 const routes: Array<RouteRecordRaw> = [
+   {
+    path: '/',
+    redirect: '/welcome'
+  },
+  {
+    path: '/welcome',
+    name: 'Welcome',
+    component: WelcomePage
+  },
   {
     path: '/',
     redirect: '/home'
@@ -11,6 +22,15 @@ const routes: Array<RouteRecordRaw> = [
     path: '/home',
     name: 'Home',
     component: HomePage
+  },
+    {
+    path: '/',
+    redirect: '/about'
+  },
+    {
+    path: '/about',
+    name: 'About',
+    component:AboutPage
   }
 ]
 
---
+++ src/views/WelcomePage.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="Welcome">
+<h1>sdasdasd</h1>
+</div>
+</template>
+
+
+<script>
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  components: {  },
+
+});
+</script>
+<style >
+.Welcome{
+    background: #000;
+    width: 100%;
+    height: 100%;
+}
+</style>
