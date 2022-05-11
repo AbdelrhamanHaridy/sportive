624c6faac82910eec87dca12ea281f5620dfb49b
Omar125Radwan <73297417+Omar125Radwan@users.noreply.github.com>
2022-05-11 13:21:59 +0200

    Cate

--- src/views/CategoryPage.vue
+++ src/views/CategoryPage.vue
@@ -12,61 +12,70 @@
     <!-- Default Searchbar -->
 
     <ion-list>
-      <ion-item lines="none">
-      <i class="fa-solid fa-dumbbell"></i>
-        <div class="content">
-          <span>Gym</span>
-          <p>06 empty place</p>
-
-          </div>
-      <i slot="end" class="fa-solid fa-arrow-right-to-bracket"></i>
-        </ion-item
-      > 
-      <ion-item lines="none">
-        <i class="fa-solid fa-person-swimming"></i>
-        <div class="content">
-          <span>Swimming</span>
-          <p>13 empty place</p>
-          </div>
-      <i slot="end" class="fa-solid fa-arrow-right-to-bracket"></i>
-        </ion-item
-      > 
-      <ion-item lines="none">
-        <i class="fa-regular fa-futbol"></i>
-        <div class="content">
-          <span>Football</span>
-          <p>05 empty place</p>
-          </div>
-      <i slot="end" class="fa-solid fa-arrow-right-to-bracket"></i>
-        </ion-item
-      > 
-      <ion-item lines="none">
-        <i class="fa-solid fa-person-running"></i>
-        <div class="content">
-          <span>Running</span>
-          <p>01 empty place</p>
-          </div>
-      <i slot="end" class="fa-solid fa-arrow-right-to-bracket"></i>
-        </ion-item
-      > 
-      <ion-item lines="none">
-        <i class="fa-solid fa-table-tennis-paddle-ball"></i>
-        <div class="content">
-          <span>Tennis</span>
-          <p>03 empty place</p>
-          </div>
-      <i slot="end" class="fa-solid fa-arrow-right-to-bracket"></i>
-        </ion-item
-      > 
-      <ion-item lines="none"
-        ><i class="fa-solid fa-volleyball"></i>
+      <ion-radio-group>
+        <ion-item lines="none">
+          <i class="fa-solid padding-i fa-dumbbell"></i>
             <div class="content">
-          <span>Volleyball</span>
-          <p>07 empty place</p>
-        </div>
-      <i slot="end" class="fa-solid fa-arrow-right-to-bracket"></i>
-        </ion-item
-      > 
+              <span>Gym</span>
+              <p>06 empty place</p>
+              </div>
+          
+          <ion-checkbox mode="ios" slot="end" value="gym"></ion-checkbox>
+        </ion-item>
+
+        <ion-item lines="none">
+          <i class="fa-solid padding-i fa-person-swimming"></i>
+          <div class="content">
+            <span>Swimming</span>
+            <p>13 empty place</p>
+          </div>
+          
+          <ion-checkbox mode="ios" slot="end" value="swimming"></ion-checkbox>
+        </ion-item> 
+
+        <ion-item lines="none">
+          <i class="fa-regular padding-i fa-futbol"></i>
+          <div class="content">
+            <span>Football</span>
+            <p>05 empty place</p>
+          </div>
+          
+          <ion-checkbox mode="ios" slot="end" value="football"></ion-checkbox>
+        </ion-item>
+
+        <ion-item lines="none">
+          <i class="fa-solid padding-i fa-person-running"></i>
+          <div class="content">
+            <span>Running</span>
+            <p>01 empty place</p>
+          </div>
+          
+          <ion-checkbox mode="ios" slot="end" value="running"></ion-checkbox>
+        </ion-item>
+
+        <ion-item lines="none">
+          <i class="fa-solid padding-i fa-table-tennis-paddle-ball"></i>
+          <div class="content">
+            <span>Tennis</span>
+            <p>03 empty place</p>
+          </div>
+          
+          <ion-checkbox mode="ios" slot="end" value="tennis"></ion-checkbox>
+        </ion-item> 
+
+
+        <ion-item lines="none">
+          <i class="fa-solid padding-i fa-volleyball"></i>
+          <div class="content">
+            <span>Volleyball</span>
+            <p>07 empty place</p>
+          </div>
+          
+          <ion-checkbox mode="ios" slot="end" value="volleyball"></ion-checkbox>
+        </ion-item> 
+
+      </ion-radio-group>
+      
     </ion-list>
     <TapComponent/>
   </ion-content>
@@ -92,23 +101,14 @@ export default defineComponent({
 });
 </script>
 <style scoped>
-ion-item {
-  padding: 20px 0;
-}
 .content {
-  padding: 0 50px;
+  padding: 0;
   font-size: 13px;
 }
-ion-item i {
+ion-item .padding-i {
   padding: 20px;
   font-size: 20px;
   background: #39bd8673;
-  border-radius: 15px;
-}
-.fa-arrow-right-to-bracket{
-    padding: 20px;
-  font-size: 20px;
-  background: none;
   border-radius: 15px;
 }
 ion-list {
@@ -118,10 +118,20 @@ ion-list {
   flex-direction: column;
   margin-top: 40px;
 }
-ion-list ion-item {
-  border: 1px solid #39bd8673;
-  margin-bottom: 10px;
-  width: 95%;
+ion-radio-group {
+  width: 100% !important;
+}
+ion-list ion-radio-group ion-item {
+  border: 2px solid #e5e9ef;
   border-radius: 10px;
+  margin: 10px auto;
+  align-items: center;
+  width: 95%;
+}
+ion-list ion-radio-group ion-item i {
+  margin: 10px 10px 10px 0;
+}
+.item-checkbox-checked {
+  border: 2px solid #39bd8673;
 }
 </style>
