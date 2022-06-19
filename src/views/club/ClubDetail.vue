ea16d71587191e966ae55d9d442cc3a14543255c
Abdelrhaman Haridy <68133113+AbdelrhamanHaridy@users.noreply.github.com>
2022-06-19 23:23:07 +0200

    v - 1.3

---
+++ src/views/club/ClubDetail.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="">
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil, perferendis distinctio eaque tenetur aspernatur exercitationem similique excepturi dolore voluptatibus deleniti. Ea commodi a minus, sed aperiam corrupti doloribus natus aliquid.</p>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import JsonPosts from "../../../public/assets/club_posts.json";
+export default defineComponent({
+data(){
+  return{
+    data:JsonPosts
+  }
+}
+});
+</script>
